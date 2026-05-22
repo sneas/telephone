c3a1795d4b117d15dd8ecaa371f5de478b3760b6
Dima Snisarenko <snisarenkodima@gmail.com>
2026-05-22 17:37:53 +0200

    docs(readme): better-looking image

--- packages/www/src/main.ts
+++ packages/www/src/main.ts
@@ -1,2 +1,19 @@
 import './style.css'
 import '@sneas/telephone';
+
+let contentToRotate = `<p>This component draws a smartphone frame around any HTML.</p>`
+
+document.body.addEventListener('keydown', (e) => {
+  if (e.key !== 'f') {
+    return;
+  }
+  const androidContent = document.getElementById('android-content');
+  if (!androidContent) {
+    return;
+  }
+
+  const currentContent = androidContent.innerHTML;
+  androidContent.innerHTML = contentToRotate;
+  contentToRotate = currentContent;
+  e.preventDefault();
+}, false)
